d9a21e81bd355009131174201526a0d0f024b4df
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2019-02-01 04:03:49 -0700

    (refactor): node environment

--- config.ts
+++ config.ts
@@ -29,7 +29,7 @@ export const config: Config = {
   USE_SSL: isTrue(process.env.USE_SSL),
   FORCE_SYNC: isTrue(process.env.FORCE_SYNC),
   PORT: process.env.PORT!,
-  ENV: process.env.NODE_ENV! as Environment || Environment.Development,
+  ENV: process.env.NODE_ENV! as Environment || Environment.Production,
   FRONT_END_URL: process.env.NODE_ENV === Environment.Test ? 'http://localhost:8081' : process.env.FRONT_END_URL!,
   STATIC_IP: isTrue(process.env.STATIC_IP),
   ALLOW_ORIGIN: process.env.ALLOW_ORIGIN ? process.env.ALLOW_ORIGIN.split(',') : ['http://twang.herokuapp.com', 'https://twang.herokuapp.com']
