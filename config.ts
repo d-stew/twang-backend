cc807c6d27a3e8b32a4b6e29adfbf6089267f004
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2019-01-31 23:54:38 -0700

    (chore): Update config

--- config.ts
+++ config.ts
@@ -5,10 +5,10 @@ dotenv.config()
 const isTrue = isEqual('true')
 
 export enum Environment {
-  Test = 'test',
-  Development = 'development',
-  Staging = 'staging',
-  Production = 'production'
+  Test = 'TEST',
+  Development = 'DEVELOPMENT',
+  Staging = 'STAGING',
+  Production = 'PRODUCTION'
 }
 
 type Config = {
@@ -29,7 +29,7 @@ export const config: Config = {
   USE_SSL: isTrue(process.env.USE_SSL),
   FORCE_SYNC: isTrue(process.env.FORCE_SYNC),
   PORT: process.env.PORT!,
-  ENV: process.env.NODE_ENV! as Environment,
+  ENV: process.env.NODE_ENV! as Environment || Environment.Development,
   FRONT_END_URL: process.env.NODE_ENV === Environment.Test ? 'http://localhost:8081' : process.env.FRONT_END_URL!,
   STATIC_IP: isTrue(process.env.STATIC_IP),
   ALLOW_ORIGIN: process.env.ALLOW_ORIGIN ? process.env.ALLOW_ORIGIN.split(',') : ['http://twang.herokuapp.com', 'https://twang.herokuapp.com']
